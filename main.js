fc725d6c4d8bd543259cd2745c1519f457aa1196
hargunrana <hargun.rana1@gmail.com>
2022-05-08 03:58:52 +0530

    ✅Hackerrank Automation 2.0

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let cTab;
 let browserOpenPromise = puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    args: ["--start-maximised"],
+    args: ["--start-fullscreen"],
 });
 
 browserOpenPromise
@@ -19,39 +19,47 @@ browserOpenPromise
     })
     .then(function (allTabs) {
         cTab = allTabs[0];
-        console.log("new tab");
+        console.log("New Tab is created");
         let VisitLoginPage = cTab.goto("http://www.hackerrank.com/auth/login/");
         return VisitLoginPage;
     })
     .then(function () {
-        console.log("hackerrank opened");
-        let emailTypePromise = cTab.type('input[name="username"]', email);
+        console.log("Hackerrank opened");
+        let emailTypePromise = cTab.type('input[name="username"]', email, {
+            delay: 100,
+        });
         return emailTypePromise;
     })
     .then(function () {
-        console.log("email is typed");
-        let passwordTypePromise = cTab.type('input[name="password"]', password);
+        console.log("Email is typed");
+        let passwordTypePromise = cTab.type(
+            'input[name="password"]',
+            password,
+            { delay: 100 }
+        );
         return passwordTypePromise;
     })
     .then(function () {
         console.log("Password is Typed");
 
         let loggedInPromise = cTab.click(
-            'button[data-analytics = "LoginPassword"]'
+            'button[data-analytics = "LoginPassword"]',
+            { delay: 100 }
         );
         return loggedInPromise;
     })
     .then(function () {
-        console.log("logged into hackerrank successfully");
+        console.log("Logged into Hackerrank Successfully");
 
         // waitAndClick will wait for enitre web page to load then will search the       selector and then will click on the node
         let openAlgoTabPromise = waitAndClick(
-            'div[data-automation="algorithms"]'
+            'div[data-automation="algorithms"]',
+            { delay: 100 }
         );
         return openAlgoTabPromise;
     })
     .then(function () {
-        console.log("Algo page is opened");
+        console.log("Algorithm page is opened");
         // resolve();
         let allQuestionPromise = cTab.waitForSelector(
             'a[data-analytics="ChallengeListChallengeName"]'
@@ -75,15 +83,16 @@ browserOpenPromise
         return linksArrPromise;
     })
     .then(function (allLinks) {
-        console.log("links to all questions received");
-        console.log(allLinks);
+        console.log("Links to all questions received");
+        //console.log(allLinks);
 
         // Solving the question
-        let quesWillBeSolvedPromise = questionSolver(allLinks[0], 0);
+        let quesWillBeSolvedPromise = questionSolver(allLinks[1], 1);
         return quesWillBeSolvedPromise;
     })
     .then(function () {
-        console.log("question is solved");
+        console.log("Question is Solved");
+        console.log("CONGRATS CODE IS PERFECT");
     })
     .catch(function (err) {
         console.log(err);
@@ -122,64 +131,101 @@ function questionSolver(url, idx) {
 
         goToQuesPagePromise
             .then(function () {
-                console.log("question opened");
+                console.log("Question Opened");
                 // Tick the custom input box mark
-                let waitForCheckBoxAndClickPromise =
-                    waitAndClick(".checkbox-input");
-                return waitForCheckBoxAndClickPromise;
-            })
-            .then(function () {
-                console.log("checkbox clicked");
-                // select the box where code will be typed
-                let waitForTextBoxPromise =
-                    cTab.waitForSelector(".custom-input");
-                return waitForTextBoxPromise;
-            })
-            .then(function () {
-                let codeWillBeTypedPromise = cTab.type(
-                    ".custom-input",
-                    answers[0]
+                let waitForSettingsPromise = cTab.waitForSelector(
+                    ".hr-monaco-settings-editor"
                 );
-                return codeWillBeTypedPromise;
-            })
-            .then(function () {
-                console.log("code typed");
-                // control key is pressed
-                let controlPressedPromise = cTab.keyboard.press("Control");
-                return controlPressedPromise;
-            })
-            .then(function () {
-                let aKeyPressedPromise = cTab.keyboard.press("a");
-                return aKeyPressedPromise;
-            })
-            .then(function () {
-                let xKeyPressedPromise = cTab.keyboard.press("x");
-                return xKeyPressedPromise;
+                return waitForSettingsPromise;
             })
             .then(function () {
                 // Select the editor
+                console.log("Disabled Autocomplete");
                 let cursorOnEditorPromise = cTab.click(
                     ".hr-monaco-editor-parent"
                 );
                 return cursorOnEditorPromise;
             })
             .then(function () {
-                let aKeyPressedPromise = cTab.keyboard.press("a");
-                return aKeyPressedPromise;
+                console.log("Clicked on Text field");
+                let ArrowDownPromise = cTab.keyboard.down("ArrowDown");
+                for (let i = 1; i < 50; i++) {
+                    ArrowDownPromise = cTab.keyboard.down("ArrowDown");
+                }
+                return ArrowDownPromise;
             })
             .then(function () {
-                let vKeyPressedPromise = cTab.keyboard.press("v");
-                return vKeyPressedPromise;
+                console.log("Moved cursor down");
+                let deleteTextPromise = cTab.keyboard.press("Backspace");
+                for (let i = 1; i < 1000; i++) {
+                    deleteTextPromise = cTab.keyboard.press("Backspace", {
+                        delay: 100,
+                    });
+                }
+                return deleteTextPromise;
             })
             .then(function () {
-                let clickSubmitPromise = cTab.click(".hr-monaco-submit");
-                return clickSubmitPromise;
+                let clickOnSettingsEditorPromise = cTab.click(
+                    ".hr-monaco-settings-editor",
+                    { delay: 100 }
+                );
+                return clickOnSettingsEditorPromise;
             })
             .then(function () {
-                console.log("submit button clicked");
-
-                let controlDownPromise = cTab.keyboard.up("Control");
-                return controlDownPromise;
+                console.log("Settings Opened");
+                let disableAutoCompletePromise = cTab.click(
+                    `button[aria-label="Disable Autocomplete"]`,
+                    { delay: 200 }
+                );
+                return disableAutoCompletePromise;
+            })
+            .then(function () {
+                let clickOnSettingsEditorPromise = cTab.click(
+                    ".hr-monaco-settings-editor",
+                    { delay: 100 }
+                );
+                return clickOnSettingsEditorPromise;
+            })
+            .then(function () {
+                // Select the editor
+                console.log("Disabled Autocomplete");
+                let cursorOnEditorPromise = cTab.click(
+                    ".hr-monaco-editor-parent"
+                );
+                return cursorOnEditorPromise;
+            })
+            .then(function () {
+                console.log("Deleted");
+                let typingPromise = cTab.type(
+                    ".hr-monaco-editor-parent",
+                    answers[0],
+                    { delay: 50 }
+                );
+                return typingPromise;
+            })
+            .then(function () {
+                console.log("Clicked on Text Field");
+                let ArrowDownPromise = cTab.keyboard.down("ArrowDown");
+                for (let i = 1; i < 3; i++) {
+                    ArrowDownPromise = cTab.keyboard.down("ArrowDown");
+                }
+                return ArrowDownPromise;
+            })
+            .then(function () {
+                console.log("Moved Cursor down");
+                let deleteTextPromise = cTab.keyboard.press("Backspace");
+                for (let i = 1; i < 3; i++) {
+                    deleteTextPromise = cTab.keyboard.press("Backspace");
+                }
+                return deleteTextPromise;
+            })
+            .then(function () {
+                console.log("Final Code Typed");
+                let clickOnSubmitButtonPromise = cTab.click(
+                    ".hr-monaco-submit",
+                    { delay: 100 }
+                );
+                return clickOnSubmitButtonPromise;
             })
             .then(function () {
                 console.log("Code submitted sucessfully");
